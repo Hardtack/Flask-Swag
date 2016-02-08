f1d652ef9dfc18243a4e520c9ac82ce4a83ac46c
Choi Geonu <6566gun@gmail.com>
2016-02-08 21:56:14 +0900

    Change version to 0.1.1-dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Swag',
-    version='0.1.0',
+    version='0.1.1-dev',
     description='Build swagger spec with Flask.',
     author='Choi Geonu',
     author_email='6566gun@gmail.com',
