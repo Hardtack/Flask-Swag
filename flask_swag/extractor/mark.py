012d1394f6c52b55d30c34c3cb155ce76cf6be98
Choi Geonu <6566gun@gmail.com>
2016-02-08 20:17:55 +0900

    Parameters should be protected.

--- flask_swag/extractor/mark.py
+++ flask_swag/extractor/mark.py
@@ -11,7 +11,22 @@ from .base import Extractor
 class MarkExtractor(Extractor):
     def get_mark(self, view):
         """Get mark object from view function."""
-        return getattr(view, '_swag', {})
+        return getattr(view, '_swag', {}).copy()
 
     def extract_others(self, view, ctx: dict):
-        return self.get_mark(view)
+        mark = self.get_mark(view)
+        mark.pop('parameters', None)
+        mark.pop('responses', None)
+        return mark
+
+    def build_parameters(self, view, param_info, ctx: dict) -> list:
+        mark = self.get_mark(view)
+        parameters = super().build_parameters(view, param_info, ctx)
+        parameters.extend(mark.get('parameters', []))
+        return parameters
+
+    def extract_responses(self, view, ctx: dict):
+        mark = self.get_mark(view)
+        if 'responses' not in mark:
+            return super().extract_responses(view, ctx)
+        return mark['responses']
